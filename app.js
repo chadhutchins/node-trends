32619d0dd73d5212cc382d7c9f4e588ac6e4bca5
geniuslinchao <geniuslinchao@gmail.com>
2013-04-11 10:55:45 +0800

    Add package.json and fix some expired function bugs
    express.bodyDecoder is changed to express.bodyParser,
    express.cookieDecoder is changed to express.cookieParser, ...

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ socket.on('connection', function(client){
 
 app.configure(function(){
   app.use(express.logger('\x1b[33m:method\x1b[0m \x1b[32m:url\x1b[0m :response-time'));
-  app.use(express.bodyDecoder());
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieDecoder());
+  app.use(express.cookieParser());
   app.use(express.session({ secret: 'keyboard cat' }));
   app.use(app.router);
-  app.use(express.staticProvider(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
   
   app.set('view engine', 'haml');
